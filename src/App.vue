9464094aaa0f4b69bb2343220ede4de5f8b4d6cc
mlavin7 <mlavin7@gmail.com>
2024-02-29 09:58:23 +0100

    Revert "mistake fixing"
    This reverts commit 9f86563512777902bf9ce8fe1f561138f8c9232b.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue"
+
+import Cards from "./components/CourseCard.vue"
+const currentDate = new Date().toDateString();
+
+let blueCard = ref({
+
+  courseDate: "5845nzjn5",
+  courseName: "somethingwww",
+  courseDescription: "somtheing wwwwelse"
+});
+
+</script>
+
+
+<template>
+  <body>
+
+    <header>
+      <h1>Wellcome To Opportunity</h1>
+      <h2>{{ currentDate }}</h2>
+    </header>
+    <main>
+      <Cards v-bind="blueCard" />
+    </main>
+
+
+    <footer>
+
+    </footer>
+  </body>
+</template>
+
+<style scoped>
+h2{
+  color: darkgray;
+}
+</style>
---
+++ src/components/CourseCard.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps(
+    {
+        courseDate: String,
+        courseName : String,
+        courseDescription: String
+    }
+)
+
+
+</script>
+<template>
+
+<div class=" card box">
+    <p class="hour">{{ courseDate }}</p>
+    <p class="name">{{ courseName }}</p>
+    <p class="description">{{ courseDescription}}</p>
+</div>
+
+</template>
+<style scoped>
+ .card{
+    display:flex;
+    flex-direction: column;
+    border: solid 1px black;
+    background-color: blue;
+    width: 400px;
+    margin-top: 50px;
+    height: 100px; 
+    justify-items: center;
+    padding: 20px;
+    gap:5px; 
+
+ }
+ p{
+    font-size: small;
+
+ }
+
+ .hour {
+    color:red;
+    font-weight:bold
+ }
+ .name{
+    font-weight: bolder;
+    color: #e1a8a9;
+ }
+ .description{
+    color: #e1a8a9
+ }
+</style>
